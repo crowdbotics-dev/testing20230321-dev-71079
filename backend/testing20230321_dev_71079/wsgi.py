deca855b8a005b1c2cb2240e6a0b448c863ab4e4
Crowdbotics <engineering@crowdbotics.com>
2023-03-21 14:08:16 +0000

    Initial commit.

---
+++ backend/testing20230321_dev_71079/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testing20230321_dev_71079 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing20230321_dev_71079.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testing20230321-dev-71079",
+  url: "https://testing20230321-dev-71079.botics.co",
+  api: "https://testing20230321-dev-71079.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
